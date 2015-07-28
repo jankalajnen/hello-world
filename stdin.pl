0d9c8c2f246716cba8153645adcf671e5166c650
Andrzej Jankowski <jankalajnen@gmail.com>
2015-07-28 18:51:32 +0200

    Added compiler flag and chomp function

--- stdin.pl
+++ stdin.pl
@@ -1,8 +1,12 @@
-#! /usr/bin/perl -w              # executes the Perl interpreter
+#! /usr/bin/perl                 # executes the Perl interpreter
+
+use strict;
+use warnings;
 
 my $foo = <STDIN>;               # <STDIN> stands for standard input
+my $bar = chomp( $foo );
 
-if (defined($foo)) {             # if - conditional statement, if (EXPR) BLOCK else BLOCK
+if ( defined( $bar ) ) {         # if - conditional statement, if (EXPR) BLOCK else BLOCK
     print "Data $foo exist\n";   # the simplest sort of print
 } else {                         # else statement - extension of the if statement
     print "Not exit\n";
