71fda9f15fe58442198c9fbaadcaf03c203b60e1
Andrzej Jankowski <jankalajnen@gmail.com>
2015-07-31 21:58:37 +0200

    Little correction

--- hello_world.c
+++ hello_world.c
@@ -1,8 +1,12 @@
-#include<stdio.h>           /* preprocessor command - standard input/output header file */
+#include<stdio.h>
+/* preprocessor command - standard input/output header file */
 
-int main ()                 /* main function where program execution begins */
+int main ()
+/* main function where program execution begins */
 {
-  printf("Hello world\n");  /* the simplest sort of print */
+  printf("Hello world\n");
+  /* the simplest sort of print */
 
-  return 0;                 /* terminates main func and returns the 0 value */
+  return 0;
+  /* terminates main func and returns the 0 value */
 }
